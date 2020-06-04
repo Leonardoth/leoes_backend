d3e8b636092b020f212ad58a470dbb9baf1aba53
lowqualityplays <lqplays@outlook.com>
2020-06-03 22:32:55 -0300

    WORKED!

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,17 +4,13 @@ const routes = require('./routes');
 require('dotenv-safe').config();
 const cors = require('cors');
 const app = express();
-console.log("Olha aqui: ", process.env.DB_URL)
-// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true})
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .catch(err => {
-        console.log(err);
-    });
+
 // teste para evitar o CORS
-// app.use(cors({
-//     origin : '*'
-// }))
+app.use(cors({
+    origin : '*'
+}))
 app.use(express.json());
 app.use(routes);
 app.listen(process.env.PORT || 3333);
